a77277cb0bbdaa2b9028f56e7eb3c700637adce9
Andrew Manderson <a.manderson@live.co.uk>
2021-02-02 10:52:46 +0000

    Last attempt at making the longitudinal model work -- unresolvable quadrature errors still persist.

--- scripts/surv-example/fit-stage-two.R
+++ scripts/surv-example/fit-stage-two.R
@@ -66,40 +66,14 @@ prefit_phi_step <- stan_model(
   "scripts/surv-example/models/submodel-two-phi-step.stan"
 )
 
-# this is a good old hack to avoid a numerical issue
-n_attempts <- 20
-attempts <- list()
-for (ii in 1 : n_attempts) {
-  attempts[[ii]] <- tryCatch(
-    expr = {
-      sampling(
-        prefit_phi_step,
-        data = stan_data,
-        chains = 1,
-        cores = 1,
-        iter = 1,
-        refresh = 0
-      )  
-    },
-    error = {
-      function(e) {
-        "integration did not converge"  
-      }
-    }
-  )
-}
-
-suppressWarnings(
-  stanfit_phi_step <- sampling(
-    prefit_phi_step,
-    data = stan_data,
-    chains = 1,
-    cores = 1,
-    iter = 1,
-    refresh = 0
-  )
+stanfit_phi_step <- sampling(
+  prefit_phi_step,
+  data = stan_data,
+  chains = 1,
+  cores = 1,
+  iter = 1,
+  refresh = 0
 )
-
 
 psi_two_names <- stanfit_phi_step@model_pars %>%
   grep(".*(beta|gamma|alpha).*", x = ., value = TRUE) %>% 
@@ -109,7 +83,7 @@ n_psi_two_pars <- length(psi_two_names)
 
 # general MCMC options
 n_stage_two_chain <- 5
-n_stage_two_iter <- 4e3 # 4e4 for min tail_ess in phi of ~500
+n_stage_two_iter <- 5e4 # 4e4 for min tail_ess in phi of ~500
 
 list_res <- mclapply(1 : n_stage_two_chain, mc.cores = 5, function(chain_id) {
   # set up containers, remember we are abind'ing over the chains
@@ -176,8 +150,16 @@ list_res <- mclapply(1 : n_stage_two_chain, mc.cores = 5, function(chain_id) {
     hazard_gamma = abs(rnorm(n = 1, mean = 0.6, sd = 0.2)),
     alpha = rnorm(n = 1, mean = 0, sd = 0.1)
   )
+  
+  flog.info(
+    sprintf("surv-fit-stage-two--chain-%d: initialised okay", chain_id), 
+    name = base_filename
+  )
 
   for (ii in 2 : n_stage_two_iter) {
+    phi_12_curr_list <- list(as.numeric(phi_12_samples[ii - 1, 1, ]))
+    phi_23_curr_list <- list(as.numeric(phi_23_samples[ii - 1, 1, ]))
+    
     # psi step
     psi_step_data <- c(
       stan_data,
@@ -196,52 +178,105 @@ list_res <- mclapply(1 : n_stage_two_chain, mc.cores = 5, function(chain_id) {
       data = psi_step_data,
       # init = list(as.list(psi_2_samples[ii - 1, 1, ])),
       chains = 1,
-      iter = 251,
-      warmup = 250
+      iter = 21,
+      warmup = 20,
+      refresh = 0
     )
 
     psi_2_samples[ii, 1, ] <- as.array(psi_step, pars = psi_two_names)
-    psi_curr_list <- as.list(psi_2_samples[ii, 1, ])
+    psi_2_curr_list <- as.list(psi_2_samples[ii, 1, ])
 
-    # phi step
-    iter_index_prop <- sample.int(n = n_iter_submodel_one, size = 1)
-    chain_index_prop <- sample.int(n = n_chain_submodel_one, size = 1)
-    phi_prop_list <- list(event_time = as.numeric(
-      submodel_one_samples[iter_index_prop, chain_index_prop, phi_12_names]
+    # phi_{1 \cap 2} step
+    phi_12_iter_index_prop <- sample.int(n = n_iter_submodel_one, size = 1)
+    phi_12_chain_index_prop <- sample.int(n = n_chain_submodel_one, size = 1)
+    phi_12_prop_list <- list(event_time = as.numeric(
+      submodel_one_samples[
+        phi_12_iter_index_prop, 
+        phi_12_chain_index_prop, 
+        phi_12_names
+      ]
     ))
     
-    log_prob_phi_step_prop <- log_prob(
+    log_prob_phi_12_step_prop <- log_prob(
       object = stanfit_phi_step,
       upars = unconstrain_pars(
         stanfit_phi_step,
         pars = c(
-          phi_prop_list,
-          psi_curr_list
+          psi_2_curr_list,
+          phi_12_prop_list,
+          long_beta = phi_23_curr_list
         )
       )
     )
 
-    log_prob_phi_step_curr <- log_prob(
+    log_prob_phi_12_step_curr <- log_prob(
       object = stanfit_phi_step,
       upars = unconstrain_pars(
         stanfit_phi_step,
         pars = c(
-          event_time = list(as.numeric(phi_12_samples[ii - 1, 1, ])),
-          psi_curr_list
+          psi_2_curr_list,
+          event_time = phi_12_curr_list,
+          long_beta = phi_23_curr_list
         )
       )
     )
 
-    log_alpha <- log_prob_phi_step_prop - log_prob_phi_step_curr
+    log_alpha_12_step <- log_prob_phi_12_step_prop - log_prob_phi_12_step_curr
 
-    if (runif(1) < exp(log_alpha)) {
-      psi_1_indices[ii, 1, ] <- c(iter_index_prop, chain_index_prop)
-      phi_12_samples[ii, 1, ] <- phi_prop_list$event_time
+    if (runif(1) < exp(log_alpha_12_step)) {
+      psi_1_indices[ii, 1, ] <- c(phi_12_iter_index_prop, phi_12_chain_index_prop)
+      phi_12_samples[ii, 1, ] <- phi_12_prop_list$event_time
+      phi_12_curr_list <- list(phi_12_prop_list$event_time) # for phi_{2 \cap 3} step
     } else {
       psi_1_indices[ii, 1, ] <- psi_1_indices[ii - 1, 1, ]
       phi_12_samples[ii, 1, ] <-  phi_12_samples[ii - 1, 1, ]
     }
+    
+    # phi_{2 \cap 3} step
+    phi_23_iter_index_prop <- sample.int(n = n_iter_submodel_three, size = 1)
+    phi_23_chain_index_prop <- sample.int(n = n_chain_submodel_three, size = 1)
+    phi_23_prop_list <- list(long_beta = as.numeric(
+      submodel_three_samples[
+        phi_23_iter_index_prop, 
+        phi_23_chain_index_prop, 
+        phi_23_names
+      ]
+    ))
+    
+    log_prob_phi_23_step_prop <- log_prob(
+      object = stanfit_phi_step,
+      upars = unconstrain_pars(
+        stanfit_phi_step,
+        pars = c(
+          psi_2_curr_list,
+          event_time = phi_12_curr_list,
+          phi_23_prop_list
+        )
+      )
+    )
 
+    log_prob_phi_23_step_curr <- log_prob(
+      object = stanfit_phi_step,
+      upars = unconstrain_pars(
+        stanfit_phi_step,
+        pars = c(
+          psi_2_curr_list,
+          event_time = phi_12_curr_list,
+          long_beta = phi_23_curr_list
+        )
+      )
+    )
+
+    log_alpha_23_step <- log_prob_phi_23_step_prop - log_prob_phi_23_step_curr
+
+    if (runif(1) < exp(log_alpha_23_step)) {
+      psi_3_indices[ii, 1, ] <- c(phi_23_iter_index_prop, phi_23_chain_index_prop)
+      phi_23_samples[ii, 1, ] <- phi_23_prop_list$long_beta
+    } else {
+      psi_3_indices[ii, 1, ] <- psi_3_indices[ii - 1, 1, ]
+      phi_23_samples[ii, 1, ] <-  phi_23_samples[ii - 1, 1, ]
+    }
+    
     if (ii %% 500 == 0) {
       flog.info(
         sprintf("surv-fit-stage-two--chain-%d: iteration-%d", chain_id, ii), 
@@ -253,8 +288,12 @@ list_res <- mclapply(1 : n_stage_two_chain, mc.cores = 5, function(chain_id) {
   inner_res <- list(
     psi_1_indices = psi_1_indices,
     phi_12_samples = phi_12_samples,
-    psi_2_samples = psi_2_samples
+    psi_2_samples = psi_2_samples,
+    phi_23_samples = phi_23_samples,
+    psi_3_indices = psi_3_indices
   )
+  
+  return(inner_res)
 })
 
 bind_named_sublists <- function(outer_list, name) {
@@ -265,6 +304,8 @@ bind_named_sublists <- function(outer_list, name) {
 psi_1_indices <- bind_named_sublists(list_res, "psi_1_indices")
 phi_12_samples <- bind_named_sublists(list_res, "phi_12_samples")
 psi_2_samples <- bind_named_sublists(list_res, "psi_2_samples")
+phi_23_samples <- bind_named_sublists(list_res, "phi_23_samples")
+psi_3_indices <- bind_named_sublists(list_res, "psi_3_indices")
 
 flog.info("surv-fit-stage-two: writing to disk", name = base_filename)
 
@@ -274,13 +315,24 @@ saveRDS(
 )
 
 saveRDS(
-  object = psi_1_indices,
-  file = "rds/surv-example/stage-two-psi-1-indices.rds"
+  object = phi_23_samples,
+  file = "rds/surv-example/stage-two-phi-23-samples.rds"
 )
 
 saveRDS(
   object = psi_2_samples,
   file = "rds/surv-example/stage-two-psi-2-samples.rds"
 )
+
+saveRDS(
+  object = psi_1_indices,
+  file = "rds/surv-example/stage-two-psi-1-indices.rds"
+)
+
+saveRDS(
+  object = psi_3_indices,
+  file = "rds/surv-example/stage-two-psi-3-indices.rds"
+)
+
 
 flog.info("surv-fit-stage-two: finished writing", name = base_filename)
