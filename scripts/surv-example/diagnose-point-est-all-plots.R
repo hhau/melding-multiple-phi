cd327aad359e21141703786321472ca74991d650
Andrew Manderson <a.manderson@live.co.uk>
2021-02-23 14:46:20 +0000

    Add full set of diagnostics for all intermediary distributions

--- scripts/surv-example/diagnose-point-est-all-plots.R
+++ scripts/surv-example/diagnose-point-est-all-plots.R
@@ -3,6 +3,9 @@ library(tidyverse)
 source("scripts/common/plot-settings.R")
 source("scripts/common/mcmc-util.R")
 
+sim_settings <- readRDS("rds/surv-example/simulation-settings-and-joint-data.rds")
+
+# psi_2 first
 point_est <- readRDS("rds/surv-example/point-est-psi-2-samples.rds")
 point_est_1_meld_23 <- readRDS(
   "rds/surv-example/point-est-1-meld-23-psi-2-samples.rds"
@@ -41,18 +44,38 @@ ggsave_halfheight(
   plot = p_3
 )
 
-# plot just for me -> 
-# plot an event time for those who experience the event
-p3m12_phi12_samples <- readRDS("rds/surv-example/point-est-3-meld-12-phi-12-samples.rds")
-sim_settings <- readRDS("rds/surv-example/simulation-settings-and-joint-data.rds")
+# next phi_12
+point_est_3_meld_12_phi_12_samples <- readRDS(
+  "rds/surv-example/point-est-3-meld-12-phi-12-samples.rds"
+)
+
 p4 <- bayesplot::mcmc_trace(
-  p3m12_phi12_samples[
+  point_est_3_meld_12_phi_12_samples[
     , 
     , 
     sprintf("event_time[%d]", which(as.logical(sim_settings$event_indicator)))
   ])
 
-ggsave(
-  filename = "plots/surv-example/just-for-me.pdf",
-  p4
+ggsave_fullpage(
+  filename = "plots/surv-example/point-est-3-meld-12-phi-12-event-only-diag.png",
+  plot = p4
+)
+
+point_est_1_meld_23_phi_23_samples <- readRDS(
+  "rds/surv-example/point-est-1-meld-23-phi-23-samples.rds"
+)
+
+p5 <- bayesplot::mcmc_trace(
+  point_est_1_meld_23_phi_23_samples[
+    , 
+    , 
+    c(
+      sprintf("beta[%d,1]", which(as.logical(sim_settings$event_indicator))),
+      sprintf("beta[%d,2]", which(as.logical(sim_settings$event_indicator)))
+    )
+  ])
+
+ggsave_fullpage(
+  filename = "plots/surv-example/point-est-1-meld-23-phi-23-event-only-diag.png",
+  plot = p5
 )
--- scripts/surv-example/diagnose-point-est-all-tables.R
+++ scripts/surv-example/diagnose-point-est-all-tables.R
@@ -1,12 +1,14 @@
 source("scripts/common/mcmc-util.R")
+source("scripts/surv-example/GLOBALS.R")
 
+# psi_2_samples
 point_est <- readRDS("rds/surv-example/point-est-psi-2-samples.rds")
 point_est_1_meld_23 <- readRDS("rds/surv-example/point-est-1-meld-23-psi-2-samples.rds")
 point_est_3_meld_12 <- readRDS("rds/surv-example/point-est-3-meld-12-psi-2-samples.rds")
 
 named_vec <- c(
-  "beta_zero" = r"{$\theta_{0}$}",
-  'beta_one' = r"{$\theta_{1}$}",
+  "theta_zero" = r"{$\theta_{0}$}",
+  'theta_one' = r"{$\theta_{1}$}",
   "hazard_gamma" = r"{$\gamma$}",
   "alpha" = r"{$\alpha$}"
 )
@@ -32,4 +34,33 @@ write_diagnostics_to_file(
   row_latex_names = named_vec,
   output_filename = "tex-input/surv-example/0102-point-est-3-meld-12-diag.tex",
   caption = r"{Numerical diagnostics for the posterior of $\psi_{2}$ obtained using the point estimate for $\phi_{2 \cap 3}$ in the survival example.}",
+)
+
+# the phis
+point_est_3_meld_12_phi_12_samples <- readRDS(
+  "rds/surv-example/point-est-3-meld-12-phi-12-samples.rds"
+)
+
+point_est_1_meld_23_phi_23_samples <- readRDS(
+  "rds/surv-example/point-est-1-meld-23-phi-23-samples.rds"
+)
+
+write_diagnostics_to_file(
+  samples_array = point_est_3_meld_12_phi_12_samples,
+  row_latex_names = c(
+    sprintf(r"{$T_{%d}$}", 1 : n_patients),
+    sprintf(r"{$\delta_{%d}$}", 1 : n_patients)
+  ),
+  output_filename = "tex-input/surv-example/0103-point-est-3-meld-12-phi-12-diag.tex",
+  caption = r"{Numerical diagnostics for $\phi_{1 \cap 2}$ when $\phi_{2 \cap 3}$ is fixed and submodels $\pd_{1}$ and $\pd_{2}$ are melded.}"
+)
+
+write_diagnostics_to_file(
+  samples_array = point_est_1_meld_23_phi_23_samples,
+  row_latex_names = c(
+    sprintf(r"{$\eta_{0, %d}$}", 1 : n_patients),
+    sprintf(r"{$\eta_{1, %d}$}", 1 : n_patients)
+  ),
+  output_filename = "tex-input/surv-example/0104-point-est-1-meld-23-phi-23-diag.tex",
+  caption = r"{Numerical diagnostics for $\phi_{2 \cap 3}$ when $\phi_{1 \cap 2}$ is fixed and submodels $\pd_{2}$ and $\pd_{3}$ are melded.}"
 )
